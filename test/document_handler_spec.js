e4eeec3d270c7a510097aae76809b8b8d8343387
zneix <zzneix@gmail.com>
2020-08-28 04:39:03 +0200

    Updated eslint rules

--- test/document_handler_spec.js
+++ test/document_handler_spec.js
@@ -5,22 +5,22 @@ const assert = require('assert');
 const DocumentHandler = require('../lib/document_handler');
 const Generator = require('../lib/key_generators/random');
 
-describe('document_handler', function() {
+describe('document_handler', function(){
 
-  describe('randomKey', function() {
+	describe('randomKey', function(){
 
-    it('should choose a key of the proper length', function() {
-      let gen = new Generator();
-      let dh = new DocumentHandler({ keyLength: 6, keyGenerator: gen });
-      assert.equal(6, dh.acceptableKey().length);
-    });
+		it('should choose a key of the proper length', function(){
+			let gen = new Generator();
+			let dh = new DocumentHandler({ keyLength: 6, keyGenerator: gen });
+			assert.equal(6, dh.acceptableKey().length);
+		});
 
-    it('should choose a default key length', function() {
-      let gen = new Generator();
-      let dh = new DocumentHandler({ keyGenerator: gen });
-      assert.equal(dh.keyLength, DocumentHandler.defaultKeyLength);
-    });
+		it('should choose a default key length', function(){
+			let gen = new Generator();
+			let dh = new DocumentHandler({ keyGenerator: gen });
+			assert.equal(dh.keyLength, DocumentHandler.defaultKeyLength);
+		});
 
-  });
+	});
 
 });

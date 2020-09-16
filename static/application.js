a446a2e3083cb4da83b992006c9881d0e423b0d2
Tanner Wilcox <twiclo@gmail.com>
2020-09-15 22:47:52 -0600

    Added a couple aliases

--- static/application.js
+++ static/application.js
@@ -335,9 +335,9 @@ class haste {
 haste.extensionMap = {
 	sh: 'bash', clike: 'c-like', coffee: 'coffeescript', cs: 'csharp', dpr: 'delphi',
 	erl: 'erlang', hs: 'haskell', js: 'javascript', kt: 'kotlin', tex: 'latex',
-	lsp: 'lisp', mk: 'makefile', md: 'markdown', mm: 'objectivec', pl: 'perl',
-	txt: 'plaintext', py: 'python', rb: 'ruby', rs: 'rust', sc: 'scala',
-	sm: 'smalltalk', ts: 'typscript', vbs: 'vbscript', html: 'xml', htm: 'xml',
+	lsp: 'lisp', mk: 'makefile', md: 'markdown', mm: 'objectivec', phptemp: 'php-template',
+	pl: 'perl',	txt: 'plaintext', py: 'python', pyrepl: 'python-repl', rb: 'ruby', rs: 'rust',
+	sc: 'scala', sm: 'smalltalk', ts: 'typscript', vbs: 'vbscript', html: 'xml', htm: 'xml',
 }
 
 
